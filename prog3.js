382e3cb6798c02ef9dfdb883a08987e3e796ef4b
lokesh8500 <lokeshpatil85001@gmail.com>
2026-05-22 06:09:15 +0530

    Add joke testing functionality in prog3.js

---
+++ prog3.js
@@ -0,0 +1,12 @@
+// Test for jokes
+let joke = "JavaScript walked into a bar....';
+let toldJoke = false;
+let $punchline = "Better watch out for those semi-colons.";
+let entage = 20;
+let result;
+if (toldJoke == true) {
+ alert($punchline);
+} else
+ alert(joke);
+}
+joke = "Knock, knock. Who's there? JavaScript....";
